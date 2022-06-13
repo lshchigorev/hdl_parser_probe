48f1a35c461461cecef411aa09dc537a1ae00706
Leonid Shchigorev <l.shchigorev90@gmail.com>
2022-06-13 23:10:20 +0300

    add reg_exp to parser

--- script/functions.py
+++ script/functions.py
@@ -1,11 +1,23 @@
-def InOutRegWireCnt(file_name, target):
+import re
+def PortCnt(file_name, target, debug):
     file = open(file_name)
 
     one_str_comment = 0
     mult_str_comment = 0
     cnt = 0
 
+    print(target)
     for row in file:
+        #reg_exp_match = '(^\s*){}(\s+)(?P<type>wire|reg)?(\s*)(?P<width>\[\w*[:]\w*)?(\s*)(?P<few_names>\w+[,])?(?P<last_name>\w+)(;)'.format(target)
+        #reg_exp_match = '(^\s*){}(\s+)(?P<type>wire|reg)?(\s*)(?P<width>\[\w*[:]\w*\])?(\s*)(?P<few_names>\w+(\s*)[,])?(\s*)(?P<last_name>\w+(\s*)[;])'.format(target)
+        #reg_exp_match = '(^\s*){}(\s+)(?P<type>wire|reg)?(\s*)(?P<width>\[\w*[:]\w*\](\s*))?((?P<few_names>\w+(\s*)[,](\s*))*)(\s*)(?P<last_name>\w+(\s*))([;])'.format(target)
+        
+        reg_exp_match = '(^\s*){}(\s+)(?P<type>wire|reg)?(\s*)(?P<width>\[\w*[:]\w*\](\s*))?(\s*)((?P<few_names>\w+(\s*)[,](\s*))*)(?P<last_name>\w+(\s*))([;])'.format(target)
+        match = re.search(reg_exp_match, row)
+        #match = re.findall(reg_exp_match, row)
+        reg_exp_one_str_comment = '(^\s*)(//)(.*)'
+        one_str_comment = re.search(reg_exp_one_str_comment, row)
+        
         if mult_str_comment:
             if '*/' in row:
                 mult_str_comment = 0
@@ -13,10 +25,18 @@ def InOutRegWireCnt(file_name, target):
         elif '/*' in row:
             mult_str_comment = 1
             continue
+        elif one_str_comment:
+            continue
 
-        elif target in row:
+        elif match:
             cnt += 1
-            print(row)
+            if debug:
+                print(match)
+                print('type group is ', match.group('type'))
+                print('width group is ', match.group('width'))
+                #print(match.group('few_names'))
+                print('last_name group is ', match.group('last_name'))
+
 
     file.close()
     return cnt
--- script/parser.py
+++ script/parser.py
@@ -9,14 +9,22 @@ except IndexError:
     #file_name = '../units/uart/uart/Uart8.v'
 
 print('Parser analyzes', file_name, 'Verilog-HDL file\n')
+debug = 1
 
 input_cnt = 0
+inout_cnt = 0
 output_cnt = 0
+str_input = "input"
+str_inout = "inout"
+str_output = "output"
 
-input_cnt = functions.InOutRegWireCnt(file_name, 'input')
-output_cnt = functions.InOutRegWireCnt(file_name, 'output')
+#input_cnt, inout_cnt, output_cnt = functions.PortCnt(file_name, debug)
+input_cnt = functions.PortCnt(file_name, str_input, debug)
+inout_cnt = functions.PortCnt(file_name, str_inout, debug)
+output_cnt = functions.PortCnt(file_name, str_output, debug)
 
 print(input_cnt, " Input(s) in file")
+print(inout_cnt, " Inout(s) in file")
 print(output_cnt, " Output(s) in file")
 
 
